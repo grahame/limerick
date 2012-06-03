8071ef686e3baa8278fa31f49a62b83b0c7e7d9d
Grahame Bowland <grahame@angrygoats.net>
2012-06-04 01:14:51 +0800

    decent start on limerick clojure port

---
+++ clj/project.clj
@@ -0,0 +1,7 @@
+(defproject limerick "1.0.0-SNAPSHOT"
+  :description "FIXME: write description"
+  :main "limerick.core"
+  :dependencies [[org.clojure/clojure "1.3.0"]
+                 [org.clojure/data.csv "0.1.2"]
+                 ])
+
---
+++ clj/src/limerick/core.clj
@@ -0,0 +1,30 @@
+(ns limerick.core)
+(require '[clojure.data.csv :as csv]
+         '[clojure.java.io :as io])
+
+(defn go [data_dir]  
+  (defn in-data [fname]
+    (str data_dir "/" fname))
+  (defn open-csv [fname]
+    ; (doseq [line (csv/read-csv in-file)]
+    ;   (println line))
+    (println fname) 
+    (with-open [in-file (io/reader (in-data fname))]
+      (let [lines (csv/read-csv in-file)]
+        (let [header (first lines)]
+          (doseq [line (rest lines)]
+            (let [xx (zipmap header line)]
+              (println xx)))))))
+  (open-csv "agency.txt")
+  (open-csv "calendar.txt")
+  (open-csv "calendar_dates.txt")
+  (open-csv "routes.txt")
+  (open-csv "stop_times.txt")
+  (open-csv "stops.txt")
+  (open-csv "trips.txt")
+)
+
+(defn -main [data_dir] 
+  (println data_dir)
+  (go data_dir)
+  )
